afc124d9e971376e6532090cb98d7d3238c1187e
ycs1m1yk <satoly4@gmail.com>
2022-08-24 06:08:37 +0900

    🎨 theme 색상 추가

--- src/styles/style.d.ts
+++ src/styles/style.d.ts
@@ -1,11 +1,15 @@
-import 'styled-components';
+import "styled-components";
 
-declare module 'styled-components'{
-    export interface DefaultTheme {
-        palette: {
-            borderGray: string,
-            gray: string,
-            black: string,
-        },
-    }
+declare module "styled-components" {
+  export interface DefaultTheme {
+    palette: {
+      borderGray: string;
+      gray: string;
+      black: string;
+      wetasphalt: string;
+      emerald: string;
+      pomegranate: string;
+      peterriver: string;
+    };
+  }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,14 @@
-import { DefaultTheme } from 'styled-components';
+import { DefaultTheme } from "styled-components";
 
 const theme: DefaultTheme = {
   palette: {
-    borderGray: '#DEE2E6',
-    gray: '#747474',
-    black: '#151618',
+    borderGray: "#DEE2E6",
+    gray: "#747474",
+    black: "#151618",
+    wetasphalt: "#34495e",
+    emerald: "#2ecc71",
+    pomegranate: "#c0392b",
+    peterriver: "#3498db",
   },
 };
 
